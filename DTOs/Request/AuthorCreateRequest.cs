b2e94695bcb8af025fae7c81f40f6a374c55db51
Hoa <hoanguyen02122002@gmail.com>
2025-09-28 21:11:22 +0700

    Danh sách, thêm, sửa, xóa, tìm kiếm, chi tiết tác giả

---
+++ DTOs/Request/AuthorCreateRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BTL_QuanLiBanSach.DTOs.Request
+{
+    public class AuthorCreateRequest
+    {
+
+        [Required(ErrorMessage = "Tên tác giả là bắt buộc")]
+        [StringLength(100, ErrorMessage = "Tên tác già không được vượt quá 100 ký tự")]
+        public string Name { get; set; } = string.Empty;
+
+    }
+}
--- Mapper/AuthorMapper.cs
+++ Mapper/AuthorMapper.cs
@@ -14,4 +14,5 @@ public class AuthorMapper
     {
         return authors.Select(ToAuthorResponse).ToList();
     }
+
 }
